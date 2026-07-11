eeee7155b7783c5e41284303ea8ab5a6af6ff1cf
gravadattha-netizen <gravadattha@gmail.com>
2026-07-11 11:40:07 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import google.generativeai as genai
 app = Flask('')
 
 # ======= [ CONFIGURATION - TOKENS & KEYS ] =======
-TG_TOKEN = "8646909789:AAFhLamWEWkqjnCd2pfjEXn5lMoBWPCejNo" 
+TG_TOKEN = "8646909789:AAEUqvptmEOvKj59UySIMuPS7yuu-CXn-Oo"
 GROUP_CHAT_ID = -1003940722388  
 GOOGLE_API_KEY = "AIzaSyAKM5IAugwBdKxrWQ__igkDwjwITW6f2kc"
 
